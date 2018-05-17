3a3d91bb703c7320791664dd7cda56cf8ce0fbbf
allen.you <allen.you@tinno.com>
2018-05-17 17:36:31 +0800

    master:miniHeadlines 图片adapter

--- app/src/main/java/com/youyan/android/headlines/ui/adapter/MiniHeadlinesAdapter.kt
+++ app/src/main/java/com/youyan/android/headlines/ui/adapter/MiniHeadlinesAdapter.kt
@@ -1,24 +1,59 @@
 package com.youyan.android.headlines.ui.adapter
 
+import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ImageView
+import android.widget.*
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.youyan.android.headlines.R
 import com.youyan.android.headlines.common.loadUrl
+import com.youyan.android.headlines.ui.model.LargeImage
 import com.youyan.android.headlines.ui.model.MiniHeadlines
 import com.youyan.android.headlines.utils.DateUtil
 import com.youyan.android.headlines.utils.LoggerUtil
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import com.youyan.android.headlines.ui.ItemDecoration.RecyclerViewDivider
+
 
 class MiniHeadlinesAdapter(ResId : Int,dataList : ArrayList<MiniHeadlines>)
     : BaseQuickAdapter<MiniHeadlines, BaseViewHolder>(ResId,dataList) {
 
     override fun convert(helper: BaseViewHolder, miniHeadlines: MiniHeadlines) {
-        LoggerUtil.i("share_url",miniHeadlines.share_url)
+        LoggerUtil.i("ThumbImage",miniHeadlines.thumb_image_list.toString())
 
         helper.getView<ImageView>(R.id.header).loadUrl(miniHeadlines.user.avatar_url)
         helper.getView<ImageView>(R.id.dislike).visibility = if (miniHeadlines.show_dislike) View.VISIBLE else View.GONE
+        if (miniHeadlines.large_image_list.isNotEmpty()){
+            LoggerUtil.i("large_image_list",miniHeadlines.large_image_list.toString())
+            /*helper.getView<ImageView>(R.id.large_image).visibility = View.VISIBLE
+            helper.getView<ImageView>(R.id.large_image).loadUrl(miniHeadlines.large_image_list[0].url)*/
+            val largeImageList = miniHeadlines.large_image_list
+            val imageRecyclerView = helper.getView<RecyclerView>(R.id.imageRecyclerView)
+            val imageAdapter = ImageAdapter(R.layout.layout_mini_headlines_image_item,largeImageList)
+            var gridLayoutManager = GridLayoutManager(mContext, 3)
+            gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    when(largeImageList.size){
+                        1 -> {
+                            return 3
+                        }
+                        2,3,4 -> {
+                            return 2
+                        }
+                        5,6,7,8,9 -> {
+                            return 1
+                        }
+                    }
+                    return 1
+                }
 
+            }
+
+            imageRecyclerView.layoutManager = gridLayoutManager
+            imageRecyclerView.adapter = imageAdapter
+
+        }
         helper.setText(R.id.username,miniHeadlines.user.name)
                 .setText(R.id.publishTime,DateUtil.convertSecond2Day(miniHeadlines.create_time.toLong(),null))
                 .setText(R.id.userCatergray,miniHeadlines.user.verified_content)
@@ -33,10 +68,14 @@ class MiniHeadlinesAdapter(ResId : Int,dataList : ArrayList<MiniHeadlines>)
                 .addOnClickListener(R.id.like)
 
 
-
-
     }
 
+    class ImageAdapter(ResId : Int,dataList : List<LargeImage>)
+        : BaseQuickAdapter<LargeImage, BaseViewHolder>(ResId,dataList){
+        override fun convert(helper: BaseViewHolder, item: LargeImage) {
+            helper.getView<ImageView>(R.id.image_item).loadUrl(item.url)
+        }
 
+    }
 
 }
